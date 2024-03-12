eead465f9c91114dd28e9aa602da56c3e45cd1bf
Micah Halter <micah@mehalter.com>
2024-03-12 14:46:53 -0400

    fix: localize temp variables

--- zsh-nvim-appname.plugin.zsh
+++ zsh-nvim-appname.plugin.zsh
@@ -1,20 +1,17 @@
-help_text="Usage: nvapp [-h|--help] [-c|--clean] [-d|--delete] [<nvapp_name>]...
-
-    -h|--help     Display this help text and exit
-    -c|--clean    Clean the installation files
-    -d|--delete   Delete the configuration directory
-"
-
 nvapp() {
   zparseopts -D -E -F - h=help -help=help c=clean -clean=clean d=delete -delete=delete || return 1
   if [ ! -z "${help}" ]; then
-    echo "${help_text}"
+    echo "Usage: nvapp [-h|--help] [-c|--clean] [-d|--delete] [<nvapp_name>]...
+
+        -h|--help     Display this help text and exit
+        -c|--clean    Clean the installation files
+        -d|--delete   Delete the configuration directory"
     return 0
   fi
   if [ $# -eq 0 ]; then; echo "Must pass neovim appname" && return 1; fi
-  nvapp_name="nvim_${1}"
+  local nvapp_name="nvim_${1}"
   shift
-  config_dir="${XDG_CONFIG_HOME:-$HOME/.config}/${nvapp_name}"
+  local config_dir="${XDG_CONFIG_HOME:-$HOME/.config}/${nvapp_name}"
   if [ ! -z "${clean}" ]; then
     rm -rf "${XDG_DATA_HOME:-$HOME/.local/share}/${nvapp_name}" \
            "${XDG_STATE_HOME:-$HOME/.local/state}/${nvapp_name}" \
